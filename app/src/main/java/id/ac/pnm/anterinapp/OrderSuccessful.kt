b2a8f29cd6e3c3537ee73c1301a7c876d4056667
rahmaxyn <kunnisofarahma06@gmail.com>
2025-12-08 14:33:54 +0700

    feat: order successful page

---
+++ app/src/main/java/id/ac/pnm/anterinapp/OrderSuccessful.kt
@@ -0,0 +1,31 @@
+package id.ac.pnm.anterinapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button // Import Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OrderSuccessful : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order_successful)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnCheckOrder = findViewById<Button>(R.id.btnCheckOrder)
+
+        btnCheckOrder.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+}
